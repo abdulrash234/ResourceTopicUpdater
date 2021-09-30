f2bf9981995771ac909a6935f8bf5655c481ed40
Abdulrashid Mohammed <abmoh@dfds.com>
2021-09-30 10:12:24 +0200

    Add project files.

---
+++ ResourceTopicUpdater/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ResourceTopicUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeListOfResources();
+        }
+
+        public static List<Resource> MakeListOfResources()
+        {
+          var reader = new StreamReader(File.OpenRead(@"C:\equipment-numbers.csv"));
+          var listOfResources = new List<Resource>();
+          reader.ReadLine();
+          while (!reader.EndOfStream)
+          {
+              
+              var line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    var values = line.Split(';');
+                    var resource = new Resource() {DeviceUrn = values[0], ResourceUrn = values[1]};
+                        listOfResources.Add(resource);
+                    
+                }
+          }
+          return listOfResources;
+        }
+    }
+    public class Resource
+    {
+        public string DeviceUrn { get; set; }
+
+        public string ResourceUrn { get; set; }
+    }
+}
